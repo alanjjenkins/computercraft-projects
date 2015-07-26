5feb61321d3c0bbb34587d2ba0e47c1043dda0e9
Alan James Jenkins <alan.james.jenkins@gmail.com>
2015-07-26 21:41:29 +0100

    Add sorter to startup updates.

--- sorter/startup.lua
+++ sorter/startup.lua
@@ -3,6 +3,10 @@
 shell.run("rm startup")
 shell.run("openp/github get demon012/computercraft-projects/master/sorter/startup.lua startup")
 
+-- update sorter
+shell.run("rm sorter")
+shell.run("openp/github get demon012/computercraft-projects/master/sorter/sorter.lua sorter")
+
 -- run commands
 shell.run("fg sorter")
 shell.run("bg")

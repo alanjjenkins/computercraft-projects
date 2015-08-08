a9ca9c64a9d332d437ea3d4bd7ae33d91f5e2e8c
Alan James Jenkins <alan.james.jenkins@gmail.com>
2015-08-08 08:11:31 +0100

    Initial new version of itemdb.

--- sorter/item.lua
+++ sorter/item.lua
@@ -1,6 +1,7 @@
 args = {...}
 ItemDB = {
     itemdb_path = 'itemdb',
+    screensize = term.getSize(),
 }
 ItemDB_mt = {__index = Item}
 
@@ -25,26 +26,69 @@ function ItemDB:saveItemDB()--{{{
     f.close()
     f = nil
 end--}}}
+function ItemDB:displayGUI()--{{{
+    local line = 0
+    for itemid, item in pairs(self.itemDB) do
+        line = line + 1
+        term.setCursorPos(1, line)
+        if item.name then
+            term.write(item.name)
+        else
+            term.write(itemid)
+        end
+
+        term.setCursorPos(self.screensize[0]-1, line)
+        if not item.action then
+            term.write("S")
+        else
+            if item.action == "store" then
+                term.write("A")
+            elseif item.action == "macerate" then
+                term.write("M")
+            elseif item.action == "smelt" then
+                term.write("S")
+            elseif item.action == "delete" then
+                term.write("D")
+            end
+        end
+    end
+end--}}}
+
 
 db = ItemDB
 db:loadItemDB()
-itemuuid = args[2] .. ":" .. args[3]
--- find item id and damage combo
-item = db.itemDB[itemuuid]
-if item then
-    if args[1] == 'smelt' then
-        item.action = 'smelt'
-    elseif args[1] == 'macerate' then
-        item.action = 'macerate'
-    elseif args[1] == 'action' then
-        if item.action then
-            print(item.action)
-        else
-            print("item action not set so it would be stored")
-        end
+
+db:displayGUI()
+while true do
+    local event, param1, param2, param3 = os.pullEvent()
+    if event == "timer" then
+    elseif event == "key" then
+        -- search the database
+        -- scroll up the database one line
+        -- scroll down the database one line
+        -- scroll up the database one page
+        -- scroll down the database one page
+        -- refresh the database
+        -- mark item to store
+        -- mark item to macerate
+        -- mark item to smelt
     end
-    db.itemDB[itemuuid] = item
-    db:saveItemDB()
-else
-    print("Item " .. itemuuid .. " not in the database")
 end
+
+-- if item then
+--     if args[1] == 'smelt' then
+--         item.action = 'smelt'
+--     elseif args[1] == 'macerate' then
+--         item.action = 'macerate'
+--     elseif args[1] == 'action' then
+--         if item.action then
+--             print(item.action)
+--         else
+--             print("item action not set so it would be stored")
+--         end
+--     end
+--     db.itemDB[itemuuid] = item
+--     db:saveItemDB()
+-- else
+--     print("Item " .. itemuuid .. " not in the database")
+-- end

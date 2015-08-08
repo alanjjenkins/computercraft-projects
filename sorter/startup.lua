efeac45625726ae730770472af077d9bdf0b64d0
Alan James Jenkins <alan.james.jenkins@gmail.com>
2015-08-08 08:14:55 +0100

    Oops apparently doing it in the master branch.

--- sorter/startup.lua
+++ sorter/startup.lua
@@ -1,15 +1,15 @@
 
 -- update startup
 shell.run("rm startup")
-shell.run("openp/github get demon012/computercraft-projects/ccsorter-gui/sorter/startup.lua startup")
+shell.run("openp/github get demon012/computercraft-projects/master/sorter/startup.lua startup")
 
 -- update sorter
 shell.run("rm sorter")
-shell.run("openp/github get demon012/computercraft-projects/ccsorter-gui/sorter/sorter.lua sorter")
+shell.run("openp/github get demon012/computercraft-projects/master/sorter/sorter.lua sorter")
 
 -- update item
 shell.run("rm item")
-shell.run("openp/github get demon012/computercraft-projects/ccsorter-gui/sorter/item.lua item")
+shell.run("openp/github get demon012/computercraft-projects/master/sorter/item.lua item")
 
 -- run commands
 shell.run("fg sorter")
